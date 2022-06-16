ed7a3d94880c826d88245530b3fcee1ccf62586c
muhayyuddin <muhayyuddin_1@hotmail.com>
2022-06-16 17:25:38 +0200

    removed extra packages

--- build/mbzirc_ign/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/galactic/src/gtest_vendor
-# Build directory: /home/muhayy/mbzirc_ws/src/build/mbzirc_ign/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/mbzirc_naive_spinning_radar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhayy/mbzirc_ws/src/mbzirc/mbzirc_custom/mbzirc_naive_spinning_radar
-# Build directory: /home/muhayy/mbzirc_ws/src/build/mbzirc_naive_spinning_radar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
