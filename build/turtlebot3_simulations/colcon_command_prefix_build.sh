67b9bf1c59295d65ce3bda405aba6fd4dbf07b86
muhayyuddin <muhayyuddin_1@hotmail.com>
2022-06-16 17:06:32 +0200

    cleanup

--- build/turtlebot3_simulations/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/muhayy/mbzirc_ws/src/install/share/turtlebot3_fake_node/package.sh"
-. "/home/muhayy/mbzirc_ws/src/install/share/turtlebot3_gazebo/package.sh"
